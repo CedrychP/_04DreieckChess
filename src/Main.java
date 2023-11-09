032c7070fe5b38c1518eb5f181039704c1365ed2
Philipp Cedrych <philipp.cedrych@compax.at>
2023-11-09 12:30:31 +0100

    Implementation

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,38 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Eingabe with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Umschalt+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Umschalt+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Strg+F8.
-            System.out.println("i = " + i);
-        }
+  public static void main(String[] args) {
+    System.out.println("Anzahl der Zeilen: ");
+    Scanner sc = new Scanner(System.in);
+    int zeilen = sc.nextInt();
+    if (zeilen>60){
+      System.out.println("Zeilen Maximum ist 60");
+      zeilen = 60;
     }
-}
+    for(int i = 0; i<= zeilen; i++){
+      String line = "*";
+      for(int o = 1; o <= i; o++){
+       line = line +"*";
+      }
+      System.out.println(line);
+    }
+
+    System.out.println("Schachbrett");
+    int[][] chessBoard = new int[8][8];
+    for (int i = 0; i < 8; i++) {
+      for (int j = 0; j < 8; j++) {
+        chessBoard[i][j] = (j + 1)+i;
+      }
+    }
+    for (int i = 0; i < 8; i++) {
+      for (int j = 0; j < 8; j++) {
+        System.out.printf("%3d ", chessBoard[i][j]);
+      }
+      System.out.println();
+
+  }
+}}
+
+
